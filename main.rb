abb31d285e09ec01a59137b3bec91ed9fe72cc1f
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2019-10-11 17:50:19 +0100

    Changed port number

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'webrick'
 require 'launchy'
-server = WEBrick::HTTPServer.new :Port => 8000
+server = WEBrick::HTTPServer.new :Port => 7000
 server.mount "/", WEBrick::HTTPServlet::FileHandler, './'
 trap('INT') { server.stop }
-Launchy.open("http://localhost:8000")
+Launchy.open("http://localhost:7000")
 server.start
